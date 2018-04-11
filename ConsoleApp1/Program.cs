86d4574e3930856b035bbb76b67091f1a1617a34
Oleksiy Nekrasov <n.507592979@gmail.com>
2018-04-11 17:40:05 +0200

    a

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputValue = String.Empty;
+            while(inputValue != "end")
+            {
+                inputValue = Console.ReadLine();
+                if (!string.IsNullOrEmpty(inputValue))
+                {
+                    char[] charArr = inputValue.ToCharArray();
+                    Array.Reverse(charArr);
+                    StringBuilder sb = new StringBuilder();
+                    for(int i = 0; i < charArr.Length;i++)
+                    {
+                        if (i == 0)
+                            Console.Write(charArr[i].ToString().ToUpper());
+                        else
+                            Console.Write(charArr[i].ToString().ToLower());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Pleas give your name.");
+                }
+            }
+        }
+    }
+}
